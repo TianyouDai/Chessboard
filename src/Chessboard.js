097457f44b4508ba378aa49cdfbe620e3af8f195
TianyouDai <ddai060@uottawa.ca>
2021-03-28 19:33:26 -0700

    build the board with black and white color

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -2,12 +2,35 @@ import React from 'react';
 import styles from "./Chessboard.module.css";
 
 class Chessboard extends React.Component {
-  render() {
+  render(){
+    let square = [];
+    for (let i = 0; i < 64; i++) {
+      square.push(<div className={this.getSquareClass(i)} key={i}></div>);
+    }
+
     return (
       <div className={styles.Chessboard}>
-      this is a chessboard
+        {square}
       </div>
     );
+  }
+
+  getSquareClass(n){
+      let color = styles.White;
+      let row = Math.floor(n / 8);
+      let col = n % 8;
+
+      if (row % 2 === 0) {
+        if (col % 2 !== 0){
+          color = styles.Black;
+        }
+      }
+      else{
+        if (col % 2 === 0){
+          color = styles.Black;
+        }
+      }
+      return `${styles.square} ${color}`
   }
 }
 
