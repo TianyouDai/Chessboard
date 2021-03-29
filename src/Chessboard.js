7a9723876dd61efe6f3c6eb880e2b67ae7eb249d
TianyouDai <ddai060@uottawa.ca>
2021-03-28 17:28:39 -0700

    create basic app of chessboard

---
+++ src/Chessboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./Chessboard.module.css";
+
+class Chessboard extends React.Component {
+  render() {
+    return (
+      <div className={styles.Chessboard}>
+      this is a chessboard
+      </div>
+    );
+  }
+}
+
+export default Chessboard;
