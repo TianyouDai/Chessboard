05b0c7c3a594c66d4ed427eed12bdf05935aa217
TianyouDai <ddai060@uottawa.ca>
2021-03-29 18:39:27 -0700

    Add pieces via FontAwesome. Place pieced randomly

--- src/Chessboard.js
+++ src/Chessboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from "./Chessboard.module.css";
+import Piece from "./Piece.js";
 
 class Chessboard extends React.Component {
   render(){
@@ -8,9 +9,27 @@ class Chessboard extends React.Component {
       square.push(<div className={this.getSquareClass(i)} key={i}></div>);
     }
 
+    let board_state_string = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    let board_state = board_state_string.split("/");
+    let pieces = [];
+    let numbers = "1,2,3,4,5,6,7,8,9";
+    let uid = 100;
+    for (let row of board_state){
+      for (let p of row){
+        if (numbers.indexOf(p) !== -1){
+          continue;
+        }
+        pieces.push(
+          <Piece key={uid} type={p}/>
+        );
+        uid += 1;
+      }
+    }
+
     return (
       <div className={styles.Chessboard}>
         {square}
+        {pieces}
       </div>
     );
   }
@@ -30,7 +49,7 @@ class Chessboard extends React.Component {
           color = styles.Black;
         }
       }
-      return `${styles.square} ${color}`
+      return `${styles.Square} ${color}`
   }
 }
 
---
+++ src/Piece.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './Piece.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChessRook, faChessBishop, faChessKnight, faChessQueen, faChessKing, faChessPawn } from '@fortawesome/free-solid-svg-icons';
+
+class Piece extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  render() {
+    let lookup = {
+      "r": faChessRook,
+      "b": faChessBishop,
+      "n": faChessKnight,
+      "q": faChessQueen,
+      "k": faChessKing,
+      "p": faChessPawn
+    };
+
+    let piece_icon = lookup[this.props.type.toLowerCase()];
+    const styles = {
+      left: `${Math.random() * 600}px`,
+      top:`${Math.random() * 600}px`,
+      color: "green",
+      position: "absolute",
+      zIndex: 1
+    };
+
+    return (
+      <div className={styles.Piece} style={styles}>
+        <FontAwesomeIcon icon={piece_icon} />
+      </div>
+    );
+  }
+}
+
+export default Piece;
